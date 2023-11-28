bd37162ca919f2a7ffac31c555528b9acda21f98
aletardio <ale.tardio99@gmail.com>
2023-11-28 16:39:41 +0100

    fixed script and completed ex. pari o dispari

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -4,26 +4,47 @@ function randomNumber(){
 }
 
 // DEFINIZIONE FUNZIONE PER STABILIRE SE LA SOMMA DEI DUE NUMERI È PARI O DISPARI
-function checkSum(num1, num2){
-
-    let result = num1 + num2; 
-
-    if (result % 2 == 0) {
-        console.log('La somma è pari');
-    }
-
-    return console.log(result);
+function isOdd(number){
+    return number % 2 === 0;
 }    
 
 // CHIEDERE ALL'UTENTE DI SCEGLIERE PARI O DISPARI
 let OddorEven = prompt('Scegliere tra pari o dispari');
-console.log(OddorEven);
 
-// CHIEDERE ALL'UTNETE DI INSERIRE UN NUMERO DA 1 a 5
+// VERIFICA DELL'UTENTE SE HA INSERITO UNA SCELTA VALIDA
+if (OddorEven !== 'pari' && OddorEven !== 'dispari'){
+    alert("Per favore, inserisci 'pari' o 'dispari'.");
+}
+
+// CHIEDERE ALL'UTENTE DI INSERIRE UN NUMERO DA 1 a 5
 let UserNumber = parseInt(prompt("Inserire un numero da 1 a 5"));
-console.log(UserNumber);
-let cpu = randomNumber();
-cosole.log(cpu); 
 
-let sum = checkSum(UserNumber, cpu);
+console.log(OddorEven);
+console.log(UserNumber);
+
+// GENERARE UN NUMERO CASUALE PER IL COMPUTER
+let ComputerNumber = randomNumber();
+
+// CALCOLARE LA SOMMA DEI DUE NUMERI
+let sum = UserNumber + ComputerNumber;
+
+alert("La somma dei numeri è: " + sum);
+console.log(sum);
+
+// DETERMINARE SE LA SOMMA È PARI O DISPARI
+let result; 
+if (sum % 2 === 0) {
+    result = 'pari';
+}
+else {
+    result = 'dispari';
+}
+
+// MOSTRA IL VINCITORE
+if(result === OddorEven) {
+    alert("Complimenti, hai vinto! La somma è " + result + ".");
+}
+else {
+    alert("Mi dispiace, hai perso. La somma è " + result + ".");
+}
 console.log(sum);
